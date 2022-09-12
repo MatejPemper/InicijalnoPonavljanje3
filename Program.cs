0aff8ec0455bc41bd1b20657b62941173570fc15
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-09-12 12:21:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje3._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+
+            Console.WriteLine("Prosjecna vrijednost ovih brojeva je: ");
+            Console.WriteLine((a +b + c + d)/4);
+
+            Console.ReadKey();  
+
+        }
+    }
+}
